39c267aba1332850df42849f70b2f242a8a121b6
Hamza7655code <mughis.rehman@yahoo.com>
2025-03-26 17:49:41 +0000

    Add files via upload

--- Activity 2.py
+++ Activity 2.py
@@ -1,19 +1,6 @@
-units = int(input("Please enter the number of units you have consumed"))
-if units < 50:
-    amount = units * 2.60
-    surcharge = 25
-
-elif units >= 100:
-    amount = units * 3.25
-    surcharge = 35
-
-elif units >= 200:
-    amount = units * 5.26
-    surcharge = 25
-
-else:
-    amount = units * 8.45
-    surcharge = 75
-
-total = amount + surcharge
-print("\nElectricity bill = %.2f"  %total)
+string = int(input("Please enter in your own string: "))
+string2 = ("")
+for i in string:
+    string2 = i + string2
+print("The original string = " , string)
+print("The reversed string = " , string2)
